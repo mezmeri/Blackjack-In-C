3e70c444789ea7a743a47a609b4fb577738aac4a
Mads Schmidt Andersen <78485659+mezmeri@users.noreply.github.com>
2024-05-02 14:08:49 +0200

    Step-by-step comments

--- BlackJack/program.c
+++ BlackJack/program.c
@@ -41,6 +41,16 @@ typedef struct
 
 // 4) Give the dealer two cards, but only reveal the first card
 
+// 5) Read the values of the players card, and ask the player to 'Hit', 'Stand' or 'Fold'
+
+// 6) If the player 'Hit's, reevalutate the cards and see if the new value is lower, equal to or higher than 21.
+
+// 6.2) If the value is lower, give the player a new chance to 'Hit', 'Stand' or 'Fold'.
+
+// 7) If the player stands, reveal the dealers second card. If the sum of the cards are lower than 17, the dealer will take another card until the sum is >= 17.
+
+// 8) If the dealers card sum is higher than the players, the player loses. If the players card is higher than the dealer or the player has hit blackjack, the player wins automatically. The goes for the dealer.
+
 int main()
 {
     return 0;
